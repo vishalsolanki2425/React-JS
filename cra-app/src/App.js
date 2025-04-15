53a60ec6929a43a4084892fc0403fb2e799cb541
vishalsolanki <vishalsolanki4880@gmail.com>
2025-04-15 13:00:47 +0530

    project update

--- cra-app/src/App.js
+++ cra-app/src/App.js
@@ -2,24 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Hello World</h1>
+            <p>Hello World as a computer program
+                A "Hello World" program is a simple program that displays the text "Hello World!" to the user.
+                It's often the first program people write when learning a new programming language.
+                It's used to introduce programmers to a language's syntax and structure.
+                It's also used to test that a computer language is installed correctly.
+                Brian Kernighan is credited with creating the "Hello World" program.
+                Hello World as other media
+                Hello World is also the name of a magazine that covers topics such as computing, data, and artificial intelligence.
+                Hello World is also the name of a 2019 Japanese animated science fiction romantic drama film.
+                Hello World is also the name of a co-living and student housing company in India. </p>
+        </div>
+    );
 }
 
 export default App;
---
+++ vite-project-User Profile Card Task/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import UserProfileCardTask from './Component/User Profile Card Task'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="profile">
+      <UserProfileCardTask name="vishal solanki" tital="web develop" email="vishalsolanki@gmail.com" contact="9909295049" gender="male"/>
+      <UserProfileCardTask name="khushal vaghasiya" tital="web develop" email="khushalvaghasiya@gmail.com" contact="9909295049" gender="male"/>
+      <UserProfileCardTask name="yogesh radhod" tital="web develop" email="yogeshradhod@gmail.com" contact="9909295049" gender="male"/>
+      <UserProfileCardTask name="khushal vaghasiya" tital="web develop" email="khushalvaghasiya@gmail.com" contact="9909295049" gender="male"/>
+      <UserProfileCardTask name="vishal solanki" tital="web develop" email="vishalsolanki@gmail.com" contact="9909295049" gender="male"/>
+      <UserProfileCardTask name="yogesh radhod" tital="web develop" email="yogeshradhod@gmail.com" contact="9909295049" gender="male"/>
+    </div>
+  )
+}
+
+export default App
